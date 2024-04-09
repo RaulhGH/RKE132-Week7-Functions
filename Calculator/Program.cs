d9c37a10643d90298814e165533badd548f67b63
Raulh <raulhaabu@gmail.com>
2024-04-09 13:42:38 +0300

    w7

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.WriteLine("Enter opertor (+/-)");
+char userOperator =char.Parse(Console.ReadLine());
+Console.WriteLine("Siseste esimene nr");
+int firstNumb = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Siseste teine nr");
+int secondNumb = Int32.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+
+    case '+' :
+        Sum(secondNumb, firstNumb);
+        break;
+
+    default:
+        Console.WriteLine("vale");
+        break;
+}
+
+
+
+
+static void Sum(int a, int b)
+    {
+    Console.WriteLine($"{a} + {b} = {a+b}");
+}
+
---
+++ Functions/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("coming or leaving? (in/out)");
+
+
+string userChoice = Console.ReadLine();
+
+if (userChoice=="in")
+{
+    PrintHello();
+}
+
+else
+{
+    PrintBy();
+}
+
+
+
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello!");
+}
+
+static void PrintBy()
+{
+    Console.WriteLine("By!");
+}
